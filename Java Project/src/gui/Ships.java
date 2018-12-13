29ad5a5cc754c1e857d228657c92c63a7fc07b15
jeroenaudenaert <jeroen@artifor.com>
2018-12-13 14:50:16 +0100

    remove comment ships

--- Java Project/src/gui/Ships.java
+++ Java Project/src/gui/Ships.java
@@ -58,8 +58,6 @@ public class Ships {
 	return direction;
 	}
 	
-	int[][] board = new int[9][9];
-	
 	// we first want to construct a shape based on the entered data, then we want to check whether the the ship can still
 	// be placed on the board
 	// ship: start_coordinate(X, Y) & end_coordinate(X,Y)
