16e67d31b2a8cee9b665cce624fc816dc78e94b3
Vincent <vincent@artifor.com>
2018-12-20 16:51:40 +0100

    Fix scoreFile error

--- Java Project/src/battleship/ScoreFile.java
+++ Java Project/src/battleship/ScoreFile.java
@@ -1,6 +1,7 @@
 package battleship;
 
 import java.io.FileWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,17 +10,29 @@ import java.util.Scanner;
 
 public class ScoreFile {
 	
+	File scores = new File("scores.txt");
 
 	//empty constructor to create an instance of this class elsewhere
 	public ScoreFile() {
-
+		initFile();
 	}
 
 
+	public void initFile() {
+		try {
+			PrintWriter printW = new PrintWriter(new FileWriter(scores, true));
+			printW.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	
 	//method to print the final score to a text file
 	public void printScore(int finalScore) {
 		try {
-			PrintWriter printW = new PrintWriter(new FileWriter("scores.txt", true));
+			PrintWriter printW = new PrintWriter(new FileWriter(scores, true));
 			printW.println(finalScore);
 			printW.close();
 
@@ -32,6 +45,10 @@ public class ScoreFile {
 	//method that consults the text file and returns the highest value in there (highscore)
 	public int highScore() {
 		int highScore = 0;
+		if(scores.length() == 0) {
+			highScore = 0;
+		} else if (scores.length() != 0) {
+			
 		try {
 			Scanner scanner = new Scanner(new FileReader("scores.txt"));
 			highScore = scanner.nextInt();
@@ -46,7 +63,8 @@ public class ScoreFile {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
-
+		}
+		
 		return highScore;	
 	}
 
